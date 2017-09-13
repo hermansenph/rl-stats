91bd126271df1cf34f57c0d2653b18f354f9d1e2
hermansenph <hermansenph@gmail.com>
2017-09-13 12:10:43 -0700

    Refactored index.html, main.js, styles.css, add rl-background.png #1

--- main.js
+++ main.js
@@ -8,30 +8,49 @@ function writeLeaderboard(array) {
     }
     leaderBoardArray.push(leaderBoardObject)
   }
-  leaderBoardArray.sort(function (a,b){
+  leaderBoardArray.sort(function (a,b) {
     return parseFloat(b.mmr) - parseFloat(a.mmr)
   })
   leaderboardUl()
 }
 
 function leaderboardUl() {
-  for (var i = 0; i < leaderBoardArray.length; i++) {
-    var createUl = document.createElement('ul')
-    var createPlace = document.createElement('li')
-    var createPlayer = document.createElement('li')
-    var createScore = document.createElement('li')
-    createUl.className = 'row'
-    createPlace.className = 'place'
-    createPlayer.className = 'player'
-    createScore.className = 'score'
-    createPlace.textContent = i + 1
-    createPlayer.textContent = leaderBoardArray[i].name
-    createScore.textContent = leaderBoardArray[i].mmr
-    createUl.appendChild(createPlace)
-    createUl.appendChild(createPlayer)
-    createUl.appendChild(createScore)
-    document.querySelector('#leaderboardbody').appendChild(createUl)
+  var createUl = document.createElement('ul')
+  var createPlace = document.createElement('li')
+  var createPlayer = document.createElement('li')
+  var createScore = document.createElement('li')
+  createUl.className = 'row'
+  createPlace.className = 'place'
+  createPlayer.className = 'player'
+  createScore.className = 'score'
+  createPlace.textContent = 'RANK'
+  createPlayer.textContent = 'NAME'
+  createScore.textContent = 'MMR'
+  createUl.appendChild(createPlace)
+  createUl.appendChild(createPlayer)
+  createUl.appendChild(createScore)
+  document.querySelector('#leader-board').appendChild(createUl)
+  for (var num = 0; num < leaderBoardArray.length; num++) {
+    leaderboardList(num)
   }
 }
 
-window.onload = writeLeaderboard(data)
+function leaderboardList(i) {
+  var createUl = document.createElement('ul')
+  var createPlace = document.createElement('li')
+  var createPlayer = document.createElement('li')
+  var createScore = document.createElement('li')
+  createUl.className = 'row'
+  createPlace.className = 'place'
+  createPlayer.className = 'player'
+  createScore.className = 'score'
+  createPlace.textContent = i + 1
+  createPlayer.textContent = leaderBoardArray[i].name
+  createScore.textContent = leaderBoardArray[i].mmr
+  createUl.appendChild(createPlace)
+  createUl.appendChild(createPlayer)
+  createUl.appendChild(createScore)
+  document.querySelector('#leader-board').appendChild(createUl)
+}
+
+addEventListener('load', writeLeaderboard(data))
